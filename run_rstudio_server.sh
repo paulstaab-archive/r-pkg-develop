3feb85bbd8e12b45782478e7e28f72b94e16e448
Paul Staab <develop@paulstaab.de>
2015-07-13 20:57:07 +0200

    add runscript

---
+++ run_rstudio_server.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+docker run -d -P -p 8787:8787 \
+  -v /home/$USER/.config/rocker:/home/$USER \
+  -v /home/$USER/Workspace:/home/$USER/Workspace \
+  -e USER=$USER -e USERID=$UID \
+  --name="rocker" \
+  --restart="always" \
+  paulstaab/r-pkg-develop
