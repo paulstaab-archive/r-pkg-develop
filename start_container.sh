e009652e25d66c1507442373e40498508ee38508
Paul Staab <develop@paulstaab.de>
2016-11-13 13:17:02 +0100

    remove old scripts

--- start_container.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-docker run -d -P -p 8787:8787 \
-  -v /home/$USER/.config/rocker:/home/$USER \
-  -v /home/$USER/Workspace:/home/$USER/Workspace \
-  -e USER=$USER -e USERID=$UID \
-  --name="r-pkg-develop" \
-  --restart="always" \
-  paulstaab/r-pkg-develop
--- update_container.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-docker stop r-pkg-develop
-docker rm r-pkg-develop
-docker pull paulstaab/r-pkg-develop
-
